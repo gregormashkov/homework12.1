f8054f439749b7e47e5821ec8b3d96e98444f750
grifo <grifoncheg1996@gmail.com>
2022-11-03 14:21:58 +0300

    сделано ДЗ

---
+++ src/Main.java
@@ -0,0 +1,43 @@
+public class Main {
+    public static void main(String[] args) {
+        // Задание 1.
+        public static int leapYear (String year) {
+            String name = "year";
+            int year = leapYear;
+            if (year % 4 == 0 && year % 100 != 0 || year % 400 == 0) {
+                System.out.println(year + "год является високосным");
+                return;
+                System.out.println(year + "год не является високосным");
+            }
+            // Задание 2.
+            public static int getClientOS (String iOS) {
+                String osName = "iOS";
+                int clientOS = getClientOS(osName);
+                if (name.equals("iOS")) {
+                    return 0;
+                } else {
+                    return 1;
+                }
+            }
+            // Задание 3.
+            public static int distance (String deliveryDays) {
+                String name = "deliveryDays"
+                int deliveryDisntance = 95;
+                int deliveryDays = 1;
+                if (deliveryDisntance > 20) {
+                    deliveryDays++;
+                }
+                if (deliveryDisntance > 60) {
+                    deliveryDays++;
+                }
+                System.out.println("Потребуется дней: " + deliveryDays);
+            }
+
+
+        }
+
+
+
+    }
+}
+// Сделано ДЗ
